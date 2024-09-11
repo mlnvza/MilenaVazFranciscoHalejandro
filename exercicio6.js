e1311466190d4f1965b24190131953e4eecd3c12
mlnvza <vazs.milena@gmail.com>
2024-09-11 08:48:42 -0300

    Delete exercicio6.js

--- exercicio6.js
+++
@@ -1,9 +0,0 @@
-function exercicio() {
-    let numero = Math.round(Math.random() * 10);
-    let resposta = "";
-    do {
-        resposta = parseInt(prompt("Tente adivinhar o número aleatório"));
-    }
-    while (resposta !== numero)
-    alert("Parabéns! Você acertou!")
-}
